190286478211ac6aeeddc545827f4c323cdf1fab
Lujille10 <l.lizardo.558927@umindanao.edu.ph>
2026-05-25 11:27:22 +0800

    Update database.config.php

--- inventory/public/database.config.php
+++ inventory/public/database.config.php
@@ -1,5 +1,5 @@
 <?php
-$SERVER_NAME = getenv('SERVER_NAME') ?: getenv('MYSQLHOST') ?: 'localhost';
-$USERNAME    = getenv('DB_USERNAME') ?: getenv('MYSQLUSER') ?: 'root';
-$PASSWORD    = getenv('DB_PASSWORD') ?: getenv('MYSQLPASSWORD') ?: '';
-$DB_NAME     = getenv('DB_NAME')     ?: getenv('MYSQLDATABASE') ?: 'railway';
+$SERVER_NAME = getenv('DB_HOST') ?: 'localhost';
+$USERNAME    = getenv('DB_USERNAME') ?: 'root';
+$PASSWORD    = getenv('DB_PASSWORD') ?: '';
+$DB_NAME     = getenv('DB_NAME') ?: 'railway';
